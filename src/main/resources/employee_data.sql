370f8b271047ed58b8807975b3783d17e66de1a1
Rudresh Kumar <rudresh.kumar@gmail.com>
2019-09-16 11:16:18 +0530

    Added initial dummy data in h2 database to employee table

---
+++ src/main/resources/employee_data.sql
@@ -0,0 +1,3 @@
+insert into employee(id,firstName,lastname,dob,gender,department) values(1,'Rudresh','Kumar','09-12-1982','Male','GTI');
+insert into employee(id,firstName,lastname,dob,gender,department) values(2,'Krishna','Kumar','07-14-1977','Male','CIB');
+insert into employee(id,firstName,lastname,dob,gender,department) values(3,'Pritam','Kumari','06-06-1990','Female','CIB');
